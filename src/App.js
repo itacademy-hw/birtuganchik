501cc1944b6bcbd0fe01ffcdd71640650982ee38
isybai <31609568+isybai@users.noreply.github.com>
2019-02-02 19:27:40 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+
+    render() {
+
+        return (
+            <div></div>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// const request = async () => {
+//     const response = await fetch('https://api.com/values/1');
+//     const json = await response.json();
+//     console.log(json);
+// }
+
+// request();
---
+++ src/components/ui/Button.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+    render() {
+        return (<button></button>);
+    }
+}
+
+export default Button;
