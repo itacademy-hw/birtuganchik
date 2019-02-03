06bd197f30083cf92584ffb239d2c1451eefaa2d
aXmeD2 <axmed@z-192-131-1-18.ip.fs.fed.us>
2019-02-03 16:14:33 +0600

    Update Login.js
    add button

--- src/publicPages/login/Login.js
+++ src/publicPages/login/Login.js
@@ -2,8 +2,22 @@ import React, { Component } from "react";
 import styled from 'styled-components';
 
 let LoginPage = styled.div `
+    *{
+        box-sizing: border-box;
+    }
     margin: 0 auto;
     width: 340px;
+    display: block;
+    font-size: 14px;
+    line-height: 1.5;
+
+    .btn_signing{
+        margin-top: 40px;
+        display: block;
+        text-align: center;
+        width: 100%;
+    }
+    }
 `;
 
 class Login extends Component {
@@ -18,10 +32,12 @@ class Login extends Component {
         return (
             <LoginPage>
             <div className="login">
-                <p>Username or email address:</p>
+                <h1>Sign in to BitTuganchik</h1>
+                <p>Username or email address</p>
                 <input value={login}/>
-                <p>Password</p>
+                <p>Password <a class="label-link" href="/password_reset">Forgot password?</a></p>
                 <input value={password}/>
+                <button className="btn_signing">Sign in</button>
             </div>
             </LoginPage>
         );
