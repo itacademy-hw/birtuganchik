a1ff53d2e4926aba1d5595ff50dd6bee09c6a2ef
aXmeD2 <axmed@z-192-131-1-18.ip.fs.fed.us>
2019-02-03 17:04:28 +0600

    Update Login.js

--- src/publicPages/login/Login.js
+++ src/publicPages/login/Login.js
@@ -17,6 +17,13 @@ let LoginPage = styled.div `
         text-align: center;
         width: 100%;
     }
+
+ 
+    li {
+        display: inline;
+        margin-right: 5px;
+        padding: 3px;
+       }
 `;
 
 class Login extends Component {
@@ -37,7 +44,14 @@ class Login extends Component {
                 <p>Password <a class="label-link" href="/password_reset">Forgot password?</a></p>
                 <input value={password}/>
                 <button className="btn_signing">Sign in</button>
-                <p class="signup">New to BTG? <a data-ga-click="Sign in, switch to sign up" href="/join?source=login">Create an account</a>.</p>
+                <p class="signup">New to BTG? <a href="/login">Create an account</a>.</p>
+            </div>
+            <div className="navigation_footer">
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contacts">Contacts</a></li>
+                </ul>
             </div>
             </LoginPage>
         );
