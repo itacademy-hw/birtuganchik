fa4d16ce36ad3bdfdb010e1555c1316c8d51c57a
aXmeD2 <axmed@MacBook-Air-Ivan.local>
2019-02-05 19:10:32 +0600

    Update Login.js

--- src/publicPages/login/Login.js
+++ src/publicPages/login/Login.js
@@ -16,6 +16,7 @@ let LoginPage = styled.div `
         display: block;
         text-align: center;
         width: 100%;
+        padding: 7px;
     }
     input {
         width: 100%;
@@ -35,41 +36,44 @@ let LoginPage = styled.div `
         float: right;
        }
 `;
-
+let person = {
+    login: 'user1',
+    password: '12345',
+}
 class Login extends Component {
-    loginData = { 
+    state = {
         login: '',
         password: '',
-     };
-
-    render() {
-        let {login, password} = this.loginData;
-        /*  signIn = () => {
-            if (this.id.login_data === login){
-                if(this.id.password_data === password ){
-                    return "Sign In Success!";
-                }
+    };
+     signIn = () => {
+        let {login, password} = this.state;
+        if (person.login === login && person.password === password){
+                alert('Sign In Success!');
             }
-        }; */
+        alert('Access deny! Check your login and password!')
+    }; 
+    render() {
+        
+
 
         return (
             <LoginPage>
-            <div className="login">
-                <h1>Sign in to BTG</h1>
-                <p>Username/email</p>
-                <input type="text" id="login_data" />
-                <p>Password   <a class="psw_reset" href="/password_reset">Forgot password?</a></p>
-                <input type="text" id="password_data"/>
-                <button className="btn_signing">Sign in</button>
-                <p class="signup">New to BTG? <a className="crt_account" href="/login">Create an account.</a></p>
-            </div>
-            <div className="navigation_footer">
-                <ul>
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contacts">Contacts</a></li>
-                </ul>
-            </div>
+                <div className="login">
+                    <h1>Sign in to BTG</h1>
+                    <p>Username/email</p>
+                    <input onChange={chengePerson = (e) => this.setState.login.value.field} type="text" id="login_data" />
+                    <p>Password   <a class="psw_reset" href="/password_reset">Forgot password?</a></p>
+                    <input onChange={(e) => this.setState.password.value.field} type="text" id="password_data"/>
+                    <button onClick={this.signIn(this.state.login.value, this.state.password.value)} className="btn_signing">Sign in</button>
+                    <p class="signup">New to BTG? <a className="crt_account" href="/login">Create an account.</a></p>
+                </div>
+                <div className="navigation_footer">
+                    <ul>
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contacts">Contacts</a></li>
+                    </ul>
+                </div>
             </LoginPage>
         );
     }
