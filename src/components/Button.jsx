0e0a1018a2e3db13455575d0182838c86bf99a24
isybai <31609568+isybai@users.noreply.github.com>
2019-02-09 19:15:19 +0600

    onclick fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,32 +9,33 @@ let Btn = styled.button`
     text-decoration: none;
     border-radius: 5px;
     border: 0;
-    :hover { 
-        opacity: .8; 
+    :hover {
+        opacity: .8;
     }
     ${props => props.login && `
         background: green;
         transition: 0.2s;
-        :hover { 
-            opacity: .8; 
+        :hover {
+            opacity: .8;
         }`
         }
     ${props => props.remove && `
         background: red;
         transition: 0.2s;
-        :hover { 
-            opacity: .8; 
-        }   
+        :hover {
+            opacity: .8;
+        }
     `}
-    
+
 `
 
 class Button extends Component {
     state = {  }
     render() {
         let { text, login, remove, onclick } = this.props;
+
         return (
-            <Btn onClick={() => onclick()} login={login} remove={remove}>{text}</Btn>
+            <Btn onClick={() => onclick && onclick()} login={login} remove={remove}>{text}</Btn>
         );
     }
 }
