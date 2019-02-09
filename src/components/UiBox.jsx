b6a8a15dd0bccf8afde947a7f7f93b21d8bbad15
isybai <31609568+isybai@users.noreply.github.com>
2019-02-09 19:56:36 +0600

    flex component

--- src/components/UiBox.jsx
+++ src/components/UiBox.jsx
@@ -1,17 +1,64 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
 
-class UiBox extends Component {
-    
+let Uibox = styled.div`
+    background-color: #fff;
+    border: 1px solid #d1d5da;
+    border-radius: 3px;
+
+    h3 {
+        font-size: 14px;
+        font-weight: 600;
+        margin: 0;
+    }
+
+    .title {
+        background-color: #f6f8fa;
+        border: 1px solid #d1d5da;
+        border-top-left-radius: 3px;
+        border-top-right-radius: 3px;
+        margin: -1px -1px 0;
+        padding: 16px;
+    }
+`;
+
+export default class UiBox extends Component {
+
     render() {
         let { title } = this.props;
         return (
-            <div>
-                <div>{title}</div>
+            <Uibox>
+                <div className="title">
+                    <h3>{title}</h3>
+                </div>
                 <div>{this.props.children}</div>
-            </div>
+            </Uibox>
         );
     }
 }
 
-export default UiBox;
+let Flexed = styled.div`
+    display: flex;
+    ${(props) => props.column && 'flex-direction: column;'}
+    ${(props) => props.centerVert && 'align-items: center;'}
+    ${(props) => props.centerHor && 'justify-content: center;'}
+    ${(props) => props.style && {...props.style}}
+`;
+
+export class Flex extends Component {
+
+    render() {
+        let { style, column, centerVert, centerHor} = this.props;
+
+        return (
+            <Flexed
+                style={style}
+                column={column}
+                centerVert={centerVert}
+                centerHor={centerHor}
+            >
+                {this.props.children}
+            </Flexed>
+        )
+    }
+}
