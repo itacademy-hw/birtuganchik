eb7e29433b4b314fd09bacf3cfae09a269b2d268
isybai <31609568+isybai@users.noreply.github.com>
2019-02-09 19:05:00 +0600

    renamed branch

---
+++ src/publicPages/signUp/SignUp.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+    state = {  }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default SignUp;
