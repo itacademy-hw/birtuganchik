475bba1e0331ee2bcf9b6f361caaabab183386cb
aXmeD2 <axmed@MacBook-Air-Ivan.local>
2019-02-21 19:42:35 +0600

    13

--- src/publicPages/signUp/SignUp.js
+++ src/publicPages/signUp/SignUp.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from 'styled-components';
 import Button from '../../components/Button';
 
-let SignUpPage = styled.div `
+let SignUpPage = styled.div`
     .bg {
         position: absolute;
         z-index: 1;
@@ -28,7 +28,7 @@ let SignUpPage = styled.div `
             "footer footer"
     }
     h1{
-        text-align: center;
+        text-align: left;
     }
     .SignUp_L {
         background-color: #b8b7e282;
@@ -39,6 +39,9 @@ let SignUpPage = styled.div `
         background-color: #b8b7e282;
         padding: 10px;
     }
+    .flexed {
+        display: flex;
+    }
     .logo {
         text-align: center;
         padding-top: 20px;
@@ -46,30 +49,32 @@ let SignUpPage = styled.div `
     .logoImg {
         width: 180px;
     }
-    input {
-        width: 40%;
-    }
     li {
         display: inline;
         margin-right: 5px;
         padding: 3px;
     }
 
-    .border { 
+    .signup_form {
+        width: 45%;
+    }
+
+    .dots {
     display: flex;
     flex-direction: column;
     align-items: center;
-    .dots {
-        width: 2px;
-        height: 40px;
-        background: blue;
-        transition: 0.3s;
+    width: 5%;
+        .dot {
+            width: 1px;
+            height: 40px;
+            background: black;
+            transition: 0.5s;
+        }
+        .dot.active {
+            transform: scale(1.4);
+            background: red;
+        }
     }
-    .dots:hover {
-        transform: scale(1.2);
-        background: red;
-    }
- }
 
     .footer {
         grid-column: 1 / span 2;
@@ -79,7 +84,7 @@ let SignUpPage = styled.div `
     }
 `;
 
-let usersData =  [];
+let usersData = [];
 
 class SignUp extends Component {
     state = {
@@ -90,91 +95,104 @@ class SignUp extends Component {
             age: undefined,
             password: undefined
         },
+        active: false,
         submitted: false
     };
 
-
+    changeInput = () => {
+            
+    };
 
     createPerson = (value, field) => {
         this.setState({
-            user: {...this.state.user, [field]: value}
+            user: { ...this.state.user, [field]: value }
         });
 
-         let emailValid = value.match(/^[-!#$%&'*+\\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/);
+        let emailValid = value.match(/^[-!#$%&'*+\\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/);
 
-         if(field === 'email'){
-             if(!!emailValid){
-                return this.setState({user: {...this.state.user, [field]: value}})
-             }
-             return console.log('Invalid Email');
+        if (field === 'email') {
+            if (!!emailValid) {
+                return this.setState({ user: { ...this.state.user, [field]: value } })
+            }
+            return console.log('Invalid Email');
         }
 
-        if(field === 'password'){
-            if(value.length > 6){
-                return this.setState({user: {...this.state.user, [field]: value}})
+        if (field === 'password') {
+            if (value.length > 6) {
+                return this.setState({ user: { ...this.state.user, [field]: value } })
             }
             return console.log('Password is too short!');
         }
         //console.log(this.state.user);
     };
-        signUp = () => {
-            usersData.push({
-                ...this.state.user
-            })
-        };
+    signUp = () => {
+        usersData.push({
+            ...this.state.user
+        })
+    };
 
     render() {
+        let { active } = this.state;
 
         return (
-           <SignUpPage>
-            <div className='bg'></div>
-            <div className="container">
-                <div className="SignUp_L">
-                <div className="logo">
-                    <img className="logoImg" src='logo.png' alt="logo"/>
-                </div>
-                <div>
-
-                </div>
-                </div>
-                <div className="SignUp_R">
-                    <div>
-                        <h1>Sign Up</h1>
-                        <p>Email</p>
-                        <input onChange={(e) => this.createPerson(e.target.value, 'email')} type="text" id="signup_username" />
-                        <p>First Name</p>
-                        <input value={this.state.value} onChange={(e) => this.createPerson(e.target.value, 'firstName')} type="text" id="signup_firstname"/>
-                        <p>Last Name</p>
-                        <input value={this.state.value} onChange={(e) => this.createPerson(e.target.value, 'lastName')} type="text" id="signup_lastname"/>
-                        <p>Age</p>
-                        <input value={this.state.value} onChange={(e) => this.createPerson(e.target.value, 'age')} type="text" id="signup_age"/>
-                        <p>Password</p>
-                        <input value={this.state.value} onChange={(e) => this.createPerson(e.target.value, 'password')} type="text" id="signup_password"/>
+            <SignUpPage>
+                <div className='bg'></div>
+                <div className="container">
+                    <div className="SignUp_L">
+                        <div className="logo">
+                            <img className="logoImg" src='logo.png' alt="logo" />
+                        </div>
+                        <div>
+                        </div>
                     </div>
-                <div className="border"> 
-                    <span  className="dots"/>
-                    <span  className="dots"/>
-                    <span  className="dots"/>
-                    <span  className="dots"/>
-                    <span  className="dots"/>
-                    <span  className="dots"/>
-                    <span  className="dots"/>
-                    <span  className="dots"/>
+                    <div className="SignUp_R">
+                        <h1>Sign Up</h1>
+                        <div className="flexed">
+                            <form className="signup_form">
+                                <p>Email</p>
+                                <input onChange={(e) => {
+                                        this.createPerson(e.target.value, 'email');
+                                        this.setState({active: true})}
+                                    } type="text" id="signup_username" />
+                                <p>First Name</p>
+                                <input onChange={(e) => {
+                                        this.createPerson(e.target.value, 'firstName');
+                                        this.setState({active: true})}
+                                        } type="text" id="signup_firstname" />
+                                <p>Last Name</p>
+                                <input onChange={(e) => this.createPerson(e.target.value, 'lastName')} type="text" id="signup_lastname" />
+                                <p>Age</p>
+                                <input onChange={(e) => this.createPerson(e.target.value, 'age')} type="text" id="signup_age" />
+                                <p>Password</p>
+                                <input onChange={(e) => this.createPerson(e.target.value, 'password')} type="text" id="signup_password" />
+                                <div>
+                                    {/*  <button onClick={() => this.signUp()} className="btn_signUp">Sign Up</button> */}
+                                    <Button onClick={() => this.signUp()} signUp={'signUp'} text={'Sign Up'} />
+                                </div>
+                            </form>
+                            <div className="dots">
+                                <span className="dot na" />
+                                <span className={`dot email ${active && 'active' | 'email'}`} />
+                                <span className="dot na" />
+                                <span className={`dot ${active && 'active'}`} />
+                                <span className="dot na" />
+                                <span className={`dot ${active && 'active'}`} />
+                                <span className="dot na" />
+                                <span className={`dot ${active && 'active'}`} />
+                                <span className="dot na" />
+                                <span className={`dot ${active && 'active'}`} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer">
+                        <ul>
+                            <li><a href="/home">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/contacts">Contacts</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    {/*  <button onClick={() => this.signUp()} className="btn_signUp">Sign Up</button> */}
-                    <Button onClick={() => this.signUp()} signUp={'signUp'} text={'Sign Up'}/>
-                </div>
-                </div>
-                <div className="footer">
-                    <ul>
-                        <li><a href="/home">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/contacts">Contacts</a></li>
-                    </ul>
-                </div>
-            </div>
-           </SignUpPage>
+            </SignUpPage>
         );
     }
 }
