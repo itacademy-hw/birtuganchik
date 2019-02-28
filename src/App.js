2f0192c398cdf351a8e0d1b390ddf82046faa762
isybai <31609568+isybai@users.noreply.github.com>
2019-02-28 20:06:48 +0600

    branch mix fixes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
 import React, { Component } from "react";
-import Tooltip from "./components/Tooltip";
-import Flash from "./components/Flash";
-import UiCard from "./components/UiCard";
 
 export default class App extends Component {
-    state = {
-        visible: false
-    }
-
-    hide = () => {
-        this.setState({
-            visible: true
-        })
-        setTimeout(() => this.setState({visible: false}),2000)
-    }
 
     render() {
-        
-    let { visible } = this.state;
+
         return (
             <div>
-                
             </div>
-            
-
-            
         );
     }
 }
-
-// webpack, reactBuild
-
-
-
-
-
-
-
-
-// const request = async () => {
-//     const response = await fetch('https://api.com/values/1');
-//     const json = await response.json();
-//     console.log(json);
-// }
-
-// request();
