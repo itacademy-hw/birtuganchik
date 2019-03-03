bbb56e01c3b4f2e47d8057fd0b47ef568c5b83d0
amankulov <45070259+amankulov@users.noreply.github.com>
2019-03-03 16:38:17 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import CardView from './components/familyView/cardView/CardView';
 
 export default class App extends Component {
 
@@ -6,6 +7,7 @@ export default class App extends Component {
 
         return (
             <div>
+                <CardView/>
             </div>
         );
     }
