15ee9a1589f5085d4bc72bc4c67b65a6225d9bf6
isybai <31609568+isybai@users.noreply.github.com>
2019-03-09 19:57:12 +0600

    3js Tree

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import RootTree from "./pages/tree/Tree";
+
+let Round = styled.div`
+    width: 50px;
+    height: 50px;
+    border: 1px solid black;
+    display: inline-block;
+    border-radius: 50%;
+    margin: 40px;
+`;
 
 export default class App extends Component {
 
     render() {
-
         return (
-            <div>
+            <div style={{ textAlign: 'center' }}>
+                <RootTree/>
             </div>
         );
     }
